76972292c65e4e07d7667b0a1ca7017494178b56
antoniomarcelino <antmarcdev@outlook.com>
2022-11-09 17:19:36 -0300

    Exercício prático simples com MVC parte 1

---
+++ app2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using app2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Inicio");
+        }
+
+        public ViewResult Formulario()
+        {
+            return View("Formulario");
+        }
+    }
+}
---
+++ app2/Models/RespostaConvidados.cs
@@ -0,0 +1,11 @@
+﻿namespace app2.Models
+{
+    public class RespostaConvidados
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+        public bool? Presente { get; set; }
+    }
+}
